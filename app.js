a1fd2659d3d343ca121ea42eab1cccc7bc65f544
Rsoniie <roshansoniee@gmail.com>
2025-01-15 19:29:16 +0530

    Cors issue solved

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ import Feedback_routes from './routes/Feed_route.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: '*' 
+}));
 const PORT = 3000
 
 // console.log("Thiese are all environment variagles", process.env.MONGODB_URI)
